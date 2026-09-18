06331cf4acf1fe6477a09902a7af1ab53eacfb48
satyamgiri9846-cmyk <satyamgiri9846@gmail.com>
2026-09-18 14:03:20 +0545

    Add about section

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <section style={{ padding: "2rem 1rem" }}>
+      <h2>About Me</h2>
+      <p>
+        I'm an aspiring web developer currently interning at an IT company, where
+  I'm learning React and building real-world projects. I enjoy turning
+  designs into working, interactive websites and I'm especially interested
+  in frontend development. This portfolio is one of the projects I'm
+  building as part of my internship.
+      </p>
+    </section>
+  );
+}
+
+export default About;
