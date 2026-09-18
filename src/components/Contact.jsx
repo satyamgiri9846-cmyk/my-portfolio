7c0184047cf398cfd0789a80c380e821eada09c0
satyamgiri9846-cmyk <satyamgiri9846@gmail.com>
2026-09-18 14:08:07 +0545

    Add contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+  return (
+    <section style={{ padding: "2rem 1rem" }}>
+      <h2>Contact</h2>
+      <p>Email: satyamgiri9846@gmail.com</p>
+      <p>GitHub: github.com/satyamgiri9846</p>
+    </section>
+  );
+}
+
+export default Contact;
