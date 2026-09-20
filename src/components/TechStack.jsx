e03650e50b5c84415f006d2997eb3b3b9bf81c65
satyamgiri9846-cmyk <satyamgiri9846@gmail.com>
2026-09-20 21:22:54 +0545

    Fix TechStack component accidentally overwritten with page wrapper code

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,7 +1,20 @@
-import TechStack from "../components/TechStack";
+import "./TechStack.css";
 
-function TechStackPage({ techs }) {
-  return <TechStack techs={techs} />;
+function TechStack({ techs }) {
+  return (
+    <section className="techstack" id="techstack">
+      <span className="section-label">What I Use</span>
+      <h2>Tech Stack</h2>
+      <div className="tech-grid">
+        {techs.map((tech, i) => (
+          <div className="tech-card" key={i}>
+            <span className="tech-icon">{tech.icon}</span>
+            <span>{tech.name}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default TechStackPage;
+export default TechStack;
